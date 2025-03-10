da1e2e667bab9c2a778d602960e0dbeb8b1751bc
Audrey <adudree@gmail.com>
2025-03-10 22:59:20 +0100

    Basic game init : set players list

--- pages/game/index.vue
+++ pages/game/index.vue
@@ -1,10 +1,23 @@
+<script setup lang="ts">
+import type { IPlayer } from '~/types/player';
+
+const gameStore = useGameStore();
+const playersList = ref<IPlayer[]>(gameStore.getPlayersList);
+
+</script>
 <template>
     <div>
         <NuxtLink to="/">
-            <div>
-                Back to home
-            </div>            
+            <button>Back to home</button>            
         </NuxtLink>
-        Game index !
+        <h2>Welcome to the game</h2>
+        <div>Players list : <span v-for="player in playersList" class="playerTag"> {{ player.name }} </span></div>
     </div>
 </template>
+<style scoped lang="scss">
+.playerTag {
+    padding: 5px;
+    margin: 5px;
+    background: #e1e1e1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,62 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/test/hello')
+import type { IPlayer } from '~/types/player';
+
+const playersList = ref<IPlayer[]>([]);
+const nameInput = ref<string>('');
+const gameStore = useGameStore();
+
+function onKeyUpEnter(newName: string) {
+    playersList.value.push({name: newName});
+    nameInput.value = "";
+}
+
+async function onStartGame() {
+    gameStore.savePlayerList(playersList.value);
+    await navigateTo('/game');
+}
+
 </script>
-
 <template>
-<div>
-    <NuxtLink to="/game">Start game</NuxtLink>
-    <NuxtLink to="/admin">Admin</NuxtLink>
-
-    <div>
-        <pre>{{ data?.hello }} </pre>
+<div class="container">
+    <div class="content">
+            <button @click="onStartGame">
+                Start game
+            </button>
+        <NuxtLink to="/admin">
+            <button>
+                Admin
+            </button>
+        </NuxtLink>
     </div>
+
+    <input type="text" v-model="nameInput" @keyup.enter="onKeyUpEnter(nameInput)" />
+
+    {{ playersList.length }} players
+
+    <div class="list-container">
+        <div v-for="player in playersList" class="player">
+            {{ player.name }}
+        </div>
+    </div>
+
 </div>
 </template>
+<style scoped lang="scss">
+.container {
+    border: 1px solid blue;
+    padding: 5px;
+    .content {
+        padding: 5px;
+        border: 1px solid red;
+    }
+}
+.list-container {
+    display: flex;
+    flex-direction: column-reverse;
+    .player {
+        border: 1px solid purple;
+        margin: 2px;
+        padding: 5px;
+    }
+}
+</style>
